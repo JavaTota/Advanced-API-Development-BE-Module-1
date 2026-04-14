07e3c1139af2729537a1da7f3beb9201e79fff1f
JavaTota <jomigone@gmail.com>
2026-04-14 17:02:55 -0500

    remove file from tracking

--- config.py
+++
@@ -1,9 +0,0 @@
-class DevelopmentConfig:
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:Jahvante.97.mysql@localhost/mechanic_shop'
-    DEBUG = True # Enable debug mode for development
-
-class TestingConfig:
-    pass
-
-class ProductionConfig:
-    pass
