3d9aa7cb464f592c1beea48c684a991c1d6407d7
JavaTota <jomigone@gmail.com>
2026-04-22 18:26:44 -0500

    Add Swagger UI integration and API documentation for Service Ticket Management

--- application/__init__.py
+++ application/__init__.py
@@ -7,6 +7,20 @@ from .blueprints.mechanics import mechanic_bp
 from .blueprints.service_tickets import service_ticket_bp
 from .blueprints.Inventory import inventory_bp
 
+
+from flask_swagger_ui import get_swaggerui_blueprint
+
+SWAGGER_URL = '/api/docs'  # URL for exposing Swagger UI (without trailing '/')
+API_URL = '/static/swagger.yaml'  # Our API URL (can of course be a local resource)
+
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "Service Ticket Management API"
+    }
+)
+
 def create_app(config_name):
     #initialize app
     app = Flask(__name__)
@@ -25,5 +39,6 @@ def create_app(config_name):
     app.register_blueprint(mechanic_bp, url_prefix='/mechanics')
     app.register_blueprint(service_ticket_bp, url_prefix='/service_tickets')
     app.register_blueprint(inventory_bp, url_prefix='/inventory')
+    app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
 
     return app
